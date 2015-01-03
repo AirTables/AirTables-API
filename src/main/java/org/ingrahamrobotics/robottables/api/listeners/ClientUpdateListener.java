da777fc3b67f7fb156f6350118ff3cec70513c34
Dabo Ross <daboross@daboross.net>
2015-01-03 14:24:23 -0800

    Slight formatting change

--- src/main/java/org/ingrahamrobotics/robottables/api/listeners/ClientUpdateListener.java
+++ src/main/java/org/ingrahamrobotics/robottables/api/listeners/ClientUpdateListener.java
@@ -6,8 +6,10 @@ import org.ingrahamrobotics.robottables.api.TableType;
 public interface ClientUpdateListener {
 
     /**
-     * Fired when a table's type changes. Typically this happens when a remote client takes over publishing a table this client used to publish.
-     * @param table Table who's type changed
+     * Fired when a table's type changes. Typically this happens when a remote client takes over publishing a table this
+     * client used to publish.
+     *
+     * @param table   Table who's type changed
      * @param oldType Type table is changing from
      * @param newType Type table is changing to
      */
